98c20c7e1d34964d2057abbff1ea881644ee03f2
Carla <carlaargentinawayar@gamail.com>
2022-10-28 14:48:57 -0300

    1-F

---
+++ EJERCICIO 1-F/README.md
@@ -0,0 +1,17 @@
+[![sensores.jpg](https://i.postimg.cc/L6wBTd77/sensores.jpg)](https://postimg.cc/mt3HLnTN)
+
+# Practica de Semana 6 (Grupo 3)
+
+
+## Estructura :
+
++ EMISOR: CARPETA QUE CONTIENE EL PROGRAMA DE LA PLACA ARDUINO DONDE ESTA CONECTADO EL MODULO EMISOR
+
++ RECEPTOR : CARPETA QUE CONTIENE EL PROGRAMA DE LA PLACA ARDUINO DONDE ESTA CONECTADO EL MODULO RECEPTOR
+
++ 1-F.pdf BREVE DESCRIPION DEL ESQUEMATICO
+
++ Lore-grupo3_f.pdsprj
+
+
+#
--- EJERCICIO 1-F/RECEPTOR/RECEPTOR.ino
+++ EJERCICIO 1-F/RECEPTOR/RECEPTOR.ino
@@ -11,13 +11,15 @@ void setup()
     Serial.println("RECEPTOR");// ENVIO MENSAJE
     vw_rx_start();       // Se inicia como receptor
     
-    pinMode(13, OUTPUT);    //Configuramos el pin del Led como SALIDA, LED INTEGRADO
-    digitalWrite(13, false);// ESTA APAGADO
+    pinMode(13, OUTPUT);    //Configuramos el pin del Led como SALIDA, LED INTEGRADO QUE SE ENCIENDE Y SE APAGA SEGUN EL CARACTER RECIBIDO
+    
+    digitalWrite(13, false);// ESTA APAGADO AL INICIAR LA PROGRAMACION
 }
-
+// VARIABLE QUE ALMACENA EL CARACTER RECIBIDO
+char dato [1];
 void loop()
 {  // CONFIGURO EL DATO A RECIBIR
-    uint8_t dato [VW_MAX_MESSAGE_LEN];
+    uint8_t dato [VW_MAX_MESSAGE_LEN];// CONFIGURO EL DATO QUE SE RECIBE PARA SU INTERPRETACION
     uint8_t datoleng=VW_MAX_MESSAGE_LEN;
  
     //verificamos si hay un dato valido en el RF
@@ -25,13 +27,13 @@ void loop()
     {  
         if( dato[0]=='1') // SI DATO ES DE VALOR 1
         {
-            Serial.println("GRUPO3");// ENVIO MENSAJE
-            digitalWrite(13, HIGH); //Encendemos el Led INTEGRADRO
-            delay(3000);
+            Serial.println("GRUPO 3");// ENVIO MENSAJE POR MONITOR SERIE
+            digitalWrite(13, HIGH); //Encendemos el Led DEL PIN 13
+            delay(500); // SE ENCIENDE POR DOS SEGUNDOS
         }
-        else 
-        {    Serial.println("         ");// NO SE ENVIA NADA
-             delay(3000);
+        if( dato[0]=='0') 
+        {    Serial.println(" SENSORES Y ACTUADORES");//  SE ENVIA LA PALABRA SENSORES Y ACTUADORES SI EL CARACTER ES OTRO VALOR
+             delay(500);// DELAY DE DOS SEGUNDOS
             digitalWrite(13, LOW); //Apagamos el Led
         }            
     }
